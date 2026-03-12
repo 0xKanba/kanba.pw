32a3edf4c22e01900ec35adb81367a19e11d044f
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-03-12 11:38:06 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -27,6 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
           <a href="/index.html" class="footer-tab" data-page="index">Kanba</a>
           <a href="/younis.html" class="footer-tab" data-page="younis">Younis</a>
           <a href="/ccr.html" class="footer-tab" data-page="ccr">Compounding Calc.</a>
+          <a href="/HLsee.html" class="footer-tab" data-page="HLsee">see analysis</a>
+          <a href="/video2026mar.html" class="footer-tab" data-page="video2026mar">videosNEWS</a>
+          
         </div>
         <div class="copyright">
           © 2025-2026 Kanba_trader | All Rights Reserved
