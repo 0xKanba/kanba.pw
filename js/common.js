5ac721672fcdbb2788cb5b1bea2c4cf511e0f9d9
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-03-14 15:35:50 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded',function(){const link=document.crea
         <div class="footer-tabs">
           <a href="/index.html" class="footer-tab" data-page="index">Kanba</a>
           <a href="/younis.html" class="footer-tab" data-page="younis">Younis</a>
+          <a href="/ccr.html" class="footer-tab" target="_blank" data-page="ccr">Compounding Calc.</a>
+          <a href="/HLsee.html" class="footer-tab" data-page="HLsee">see analysis</a>
+          <a href="/video2026mar.html" class="footer-tab" data-page="video2026mar">video2026mar</a>
 
         </div>
         <div class="copyright">
