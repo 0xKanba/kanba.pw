2542215dec696e4c3c16daeb70aded7b3d9e4ddc
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-03-14 15:38:20 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded',function(){const link=document.crea
         <div class="footer-tabs">
           <a href="/index.html" class="footer-tab" data-page="index">Kanba</a>
           <a href="/younis.html" class="footer-tab" data-page="younis">Younis</a>
-          <a href="/ccr.html" class="footer-tab" target="_blank" data-page="ccr">Compounding Calc.</a>
-          <a href="/HLsee.html" class="footer-tab" data-page="HLsee">see analysis</a>
-          <a href="/video2026mar.html" class="footer-tab" data-page="video2026mar">video2026mar</a>
+          <a href="/ccr.html" class="footer-tab"  data-page="ccr">Compounding Calc.</a>
+          <a href="/HLsee.html" class="footer-tab" data-page="see analysis">see analysis</a>
+          <a href="/video2026mar.html" class="footer-tab" target="_blank" data-page="video2026mar">video2026mar</a>
 
         </div>
         <div class="copyright">
@@ -16,5 +16,5 @@ document.addEventListener('DOMContentLoaded',function(){const link=document.crea
         </div>
       </div>
     </footer>
-  `;const headerElement=document.getElementById('header');const footerElement=document.getElementById('footer');if(headerElement)headerElement.innerHTML=headerHTML;if(footerElement)footerElement.innerHTML=footerHTML;setTimeout(()=>{const path=window.location.pathname.toLowerCase().trim();let pageKey=path.split('/').pop().replace(/\.html$/,'').replace(/\/$/,'').trim();if(!pageKey||pageKey==='en'){pageKey='index'}
+  `;const headerElement=document.getElementById('header');const footerElement=document.getElementById('footer');if(headerElement)headerElement.innerHTML=headerHTML;if(footerElement)footerElement.innerHTML=footerHTML;setTimeout(()=>{const path=window.location.pathname.toLowerCase().trim();let pageKey=path.split('/').pop().replace(/\.html$/,'').replace(/\/$/,'').trim();if(!pageKey||pageKey==='index.html'){pageKey='index'}
 document.querySelectorAll('.footer-tab').forEach(tab=>{const tabPage=tab.getAttribute('data-page');if(tabPage===pageKey){tab.classList.add('active')}})},150)})
