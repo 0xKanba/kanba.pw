a06e4338d870abf21c46e2687527e8a88f7d98ac
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-07-18 23:07:57 +0300

    Update cache version to v10

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = "kanba-v9";
-const IMAGE_CACHE = "kanba-images-v9";
-const STATIC_CACHE = "kanba-static-v9";
+const CACHE_VERSION = "kanba-v10";
+const IMAGE_CACHE = "kanba-images-v10";
+const STATIC_CACHE = "kanba-static-v10";
 
 // ملفات أساسية (لا تضع كل المشروع هنا)
 const STATIC_ASSETS = [
